9675940c5397578d20b4adc1d49346c8c5e39b34
Nathalia <110792289+ndamasc@users.noreply.github.com>
2025-03-03 17:11:13 -0400

    add mais informacoes na pagina

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
-    lista = ['Tetris', 'Skyrim', 'Clash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atalho')
+    jog2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jog3 = Jogo('Mortal Combat', 'Luta', 'PS2')
+    lista = [jogo1, jog2, jog3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
