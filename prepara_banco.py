3e70c77519bd452b3c4f61e9f875e6c7f3fb41f7
Nathalia <110792289+ndamasc@users.noreply.github.com>
2025-03-16 18:18:29 -0400

    modificando arquivo de bd

--- prepara_banco.py
+++ prepara_banco.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from flask_bcrypt import generate_password_hash
 from mysql.connector import errorcode
 
 print("Conectando...")
@@ -58,9 +59,9 @@ for tabela_nome in TABLES:
 # inserindo usuarios
 usuario_sql = 'INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
 usuarios = [
-      ("Bruno Divino", "BD", "alohomora"),
-      ("Camila Ferreira", "Mila", "paozinho"),
-      ("Guilherme Louro", "Cake", "python_eh_vida")
+      ("Bruno Divino", "BD", generate_password_hash("alohomora").decode('utf-8')),
+      ("Camila Ferreira", "Mila", generate_password_hash("paozinho").decode('utf-8')),  
+      ("Guilherme Louro", "Cake", generate_password_hash("flaskehlegal").decode('utf-8')),  
 ]
 cursor.executemany(usuario_sql, usuarios)
 
